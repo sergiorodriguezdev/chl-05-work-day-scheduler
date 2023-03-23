e0761f0eafab5b4bc8b4389ed73094798856f7a6
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-03-22 20:26:39 -0600

    Add Save button listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+// var arrScheduleItems = [];
+// var todaySchedule = {
+//   date: dayjs().format("MM/DD/YYYY")
+// };
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -24,13 +29,12 @@ $(function () {
   // Generate time slots as HTML elements
   // This will also apply the correct class to each time slot (past/present/future)
   createHTML();
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
+  // Grab main container
+  var containerEl = $(".container-lg");
+
+  // Add on click listener
+  containerEl.on("click", ".saveBtn", saveScheduleItems);
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -52,10 +56,10 @@ $(function () {
 function createHTML() {
 
   // Grab main container
-  var containerEl = $("#schedule");
+  var containerEl = $(".container-lg");
 
   // Create time slot DIVs - start at 9 (aka 9AM) and end with 17 (aka 5PM)
-  for (var i = 9; i < 18; i++) {
+  for (var i = 9; i <= 17; i++) {
 
     // Create <div id="hour-12" class="row time-block"></div>
     var divTimeSlot = $("<div>");
@@ -119,5 +123,17 @@ function getBackgroundColorClass(timeSlotId) {
   } else {
     return "future"
   }
+
+}
+
+function saveScheduleItems(event) {
+
+  var saveBtn = $(this);
+  var saveBtnParent = saveBtn.parent(".row");
+  
+  var timeSlotId = saveBtnParent.attr("id");
+  var textAreaDescription = saveBtnParent.children(".description")[0];
+
+  localStorage.setItem(timeSlotId, textAreaDescription.value.trim());
 
 }
